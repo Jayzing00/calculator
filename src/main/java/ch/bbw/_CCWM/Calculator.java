8050c32d432f9197eec926aad23cf2809028e2a6
Jayzing00 <jayleo2106@gmail.com>
2018-12-20 21:09:24 +0100

    Added summe Class in Calculator.java

---
+++ src/main/java/ch/bbw/_CCWM/Calculator.java
@@ -0,0 +1,17 @@
+package ch.bbw._CCWM;
+
+/**
+ * Calculator
+ * @author Jay
+ * @version 1.0.0
+ */
+
+public class Calculator {
+
+	public int summe(int summand1, int summand2) {
+
+		return summand1 + summand2;
+
+	}
+
+}
