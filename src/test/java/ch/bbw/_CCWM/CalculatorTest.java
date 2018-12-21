3c3f794096ce40094bf4e9f7b50bef88b241e63f
Jayzing00 <jayleo2106@gmail.com>
2018-12-21 08:36:42 +0100

    Added expected test for division

--- src/test/java/ch/bbw/_CCWM/CalculatorTest.java
+++ src/test/java/ch/bbw/_CCWM/CalculatorTest.java
@@ -30,5 +30,11 @@ public class CalculatorTest {
 	public void testDifferenzZweiPositiveIsOk() {
 		assertTrue(testee.differenz(25, 10) == 15);
 	}
+	
+	@Test(expected = ArithmeticException.class)
+	public void testDivisionArithmeticException() {
+		testee.division(10, 0);
+	}
+
 
 }
